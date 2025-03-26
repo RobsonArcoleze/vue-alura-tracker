2b44ffcc429efaaa7371f5a8de8c6bd2f28666b2
RobsonArcoleze <robson.arcoleze29@gmail.com>
2025-03-26 19:33:04 -0300

    Created project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import BarraLateral from './components/BarraLateral.vue'
+import Formulario from './components/Formulario.vue'
+</script>
+
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <Formulario />
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa!">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <section>00:00:00</section>
+    <button class="button">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
